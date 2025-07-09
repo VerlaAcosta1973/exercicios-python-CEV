1134549d268b8afe33fc9c0568cdba6029c9b294
VerlaAcosta1973 <verlaacostaramos1973@gmail.com>
2025-07-08 21:46:40 -0300

    terminado desafios 001,002,003

---
+++ desafio001.py
@@ -0,0 +1,4 @@
+# script que lê o nome do usuário e imprime uma mensagem de boas vindas
+
+nome = input('Qual o seu nome? ')
+print(f'Olá {nome}! Seja bem vindo ao mundo Python!')
---
+++ desafio003.py
@@ -0,0 +1,7 @@
+#script que lê dois números e imprime a soma deles
+
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
+soma = num1 + num2
+
+print(f'A soma de {num1} e {num2} é: {soma:.2f}')
